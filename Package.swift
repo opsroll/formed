292c7fea1ae76e859a162a8ab8c3d35439d15d75
Charles Gardner <charles@opsroll.com>
2018-04-28 11:23:19 -0700

    Added behrang/YamlSwift, and fixed reference error.

--- Package.swift
+++ Package.swift
@@ -7,8 +7,9 @@ let package = Package(
     name: "formed",
     dependencies: [
         .package(url: "https://github.com/jakeheis/SwiftCLI", from: "5.0.0"),
+        .package(url: "https://github.com/behrang/YamlSwift", from: "3.4.3"),
     ],
     targets: [
-        .target(name: "formed", dependencies: ["SwiftCLI"]),
+        .target(name: "formed", dependencies: ["SwiftCLI", "Yaml"]),
     ]
 )
